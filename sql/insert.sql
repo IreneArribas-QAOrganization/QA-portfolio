577c6594ab81d2a30609274d41799e8aedacd971
Irene Arribas Méndez <irenearribasmendez@gmail.com>
2026-05-15 20:54:45 +0200

    Create insert.sql
    Insert SQL Data

---
+++ sql/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO formirenearribas (name, email, subject, age, message)
+VALUES
+('Ana',	'ana@gmail.com',	'information', NULL, 'hola testing'),
+(NULL,	'pedro@gmail.com',	'job',	20, 'pedro mensaje'),
+('María',	'maria@yahoo.com',	'other', 25,	'mensaje de prueba'),
+('Ana',	'ana.work@gmail.com',	'job', 30,	'test message'),
+(NULL,	'user1@gmail.com',	'information', 20,	'mensaje testing');
