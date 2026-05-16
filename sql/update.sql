6d110cddf356a63181369039167da47363415adc
Irene Arribas Méndez <irenearribasmendez@gmail.com>
2026-05-17 00:49:45 +0200

    Update.sql

---
+++ sql/update.sql
@@ -0,0 +1,15 @@
+--Cambiar el email de María por mariatest@gmail.com
+UPDATE formirenearribas
+SET email = 'mariatest@gmail.com'
+WHERE id = 3;
+
+--Cambiar todos con edad 20 por 21
+UPDATE formirenearribas
+SET age = 21
+WHERE age =20;
+
+--Eliminar todos los registros con mensaje que contenga test
+DELETE FROM formirenearribas WHERE message LIKE '%test%';
+
+--Eliminar todos los registros con email pedro@gmail.com
+DELETE FROM formirenearribas WHERE email = 'pedro@gmail.com';
